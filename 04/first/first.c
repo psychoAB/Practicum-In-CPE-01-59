1c56bd2382a3b33a011de8c7d0801886f0e6fec9
psychoAB <dome.2499@hotmail.com>
2016-09-09 09:54:57 +0700

    init first

---
+++ 04/first/first.c
@@ -0,0 +1,19 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    PORTD = 0b00000000;
+    DDRD = 0b00001000;
+
+    while(1)
+    {
+        PORTD = 0b00001000;
+        _delay_ms(1000);
+        PORTD = 0b00000000;
+        _delay_ms(1000);
+    }
+    return 0;
+}
