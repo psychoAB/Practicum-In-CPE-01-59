9089450acbb7c9a3970c1a533ca068dc47bd9762
psychoAB <dome.2499@hotmail.com>
2016-09-21 02:42:10 +0700

    cRush/07: complete requirement

--- 05/cRush/07/07.c
+++ 05/cRush/07/07.c
@@ -4,27 +4,29 @@ int x;
 
 void func1()
 {
-  printf("In func1(), x=%d\n", x);
+    int x = 1111;
+    printf("In func1(), x=%d\n", x);
 }
 
 void func2()
 {
-  printf("In func2(), x=%d\n", x);
+    x = 2222;
+    printf("In func2(), x=%d\n", x);
 }
 
 void func3(int x)
 {
-  printf("In func3(), x=%d\n", x);
+    printf("In func3(), x=%d\n", x);
 }
 
 int main()
 {
-  x = 5678;
-  printf("In main(), x=%d\n", x);
-  func1();
-  printf("In main() after func1(), x=%d\n", x);
-  func2();
-  printf("In main() after func2(), x=%d\n", x);
-  func3();
-  printf("In main() after func3(), x=%d\n", x);
+    x = 5678;
+    printf("In main(), x=%d\n", x);
+    func1();
+    printf("In main() after func1(), x=%d\n", x);
+    func2();
+    printf("In main() after func2(), x=%d\n", x);
+    func3(3333);
+    printf("In main() after func3(), x=%d\n", x);
 }
