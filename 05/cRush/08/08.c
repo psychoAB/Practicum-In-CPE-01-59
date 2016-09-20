81f2b1d853b5ca08d9a855745271ea23b388e7c6
psychoAB <dome.2499@hotmail.com>
2016-09-21 02:58:54 +0700

    cRush/08: complete requirement

--- 05/cRush/08/08.c
+++ 05/cRush/08/08.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+    int maxLoad, passenger, currentWeight;
 
+    maxLoad  = passenger = currentWeight = 0;
+
+    printf("Enter the maximum weight in kg: ");
+    scanf("%d", &maxLoad);
+    while(currentWeight < maxLoad)
+    {
+        int weight;
+        printf("Enter passenger's weight in kg: ");
+        scanf("%d", &weight);
+        currentWeight += weight;
+        if(currentWeight <= maxLoad)
+        {
+            passenger++;
+        }
+    }
+    printf("The elevator can carry %d passenger(s)\n", passenger);
     return 0;
 }
