0f21fcf5fabb5bbf40487348ec63403fc16b3b85
psychoAB <dome.2499@hotmail.com>
2016-10-14 12:11:08 +0700

    week 09: complete light-3.c

--- 09/light-3.c
+++ 09/light-3.c
@@ -4,8 +4,21 @@
 int main()
 {
     init_peripheral();
-    while (1)
+    uint16_t lightAnalog = 0;
+    while(1)
     {
-
+        lightAnalog = read_adc(PC4);
+        if(lightAnalog > 682)
+        {
+            set_led_value(4);
+        }
+        else if(lightAnalog > 341)
+        {
+            set_led_value(2);
+        }
+        else
+        {
+            set_led_value(1);
+        }
     }
 }
