42fbf5913866800505c8851f7ba90060ead2c08f
psychoAB <dome.2499@hotmail.com>
2016-10-14 11:43:40 +0700

    week 09: complete sw-count.c

--- 09/sw-count.c
+++ 09/sw-count.c
@@ -1,11 +1,23 @@
 #include <avr/io.h>
+#include <util/delay.h>
 #include "lib/peri.h"
 
 int main()
 {
     init_peripheral();
+    int8_t number = 0;
     while (1)
     {
-
+        while(IS_SWITCH_PRESSED())
+        {
+            number++;
+            if(number >7)
+            {
+                number = 0;
+            }
+            set_led_value(number);
+            _delay_ms(300);
+            while(IS_SWITCH_PRESSED()){}
+        }
     }
 }
