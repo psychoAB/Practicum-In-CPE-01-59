3877ac4a6c7fcb195fd57071a881ee06051b8804
psychoAB <dome.2499@hotmail.com>
2016-10-14 11:07:40 +0700

    week 09: complete count.c

--- 09/count.c
+++ 09/count.c
@@ -5,8 +5,15 @@
 int main()
 {
     init_peripheral();
+    int8_t number = 0;
     while (1)
     {
-
+        if(number >7)
+        {
+            number = 0;
+        }
+        set_led_value(number);
+        _delay_ms(500);
+        number++;
     }
 }
