4293781ae1b87aea3bbeab73bc9e1fb084e6c79d
psychoAB <dome.2499@hotmail.com>
2016-10-14 12:08:27 +0700

    week 09: add read_adc()

--- 09/lib/peri.h
+++ 09/lib/peri.h
@@ -3,3 +3,4 @@
 void init_peripheral();
 void set_led(uint8_t,uint8_t);
 void set_led_value(uint8_t);
+uint16_t read_adc(uint8_t channel);
--- 09/peri.c
+++ 09/peri.c
@@ -26,3 +26,14 @@ void set_led_value(uint8_t value)
 {
     PORTC = (PORTC & 0b11111000) + (value & 0b00000111);
 }
+
+uint16_t read_adc(uint8_t channel)
+{
+    ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR) | (channel & 0b1111);
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADSC);
+
+    while((ADCSRA & (1 << ADSC)))
+        ;
+    
+    return ADCL + ADCH*256;
+}
