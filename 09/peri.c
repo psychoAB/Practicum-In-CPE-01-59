03a3174860eed0229e96eb020623eb0461410bd7
psychoAB <dome.2499@hotmail.com>
2016-10-14 10:23:46 +0700

    init week 09 lab

---
+++ 09/peri.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include "peri.h"
+
+void init_peripheral()
+{
+  // copy จาก elab
+}
+
+void set_led(uint8_t pin,uint8_t state)
+{
+  // copy จาก elab
+}
+
+void set_led_value(uint8_t value)
+{
+  // copy จาก elab
+}
---
+++ 09/peri.h
@@ -0,0 +1,5 @@
+#define IS_SW_PRESSED() // copy จาก elab
+
+void init_peripheral();
+void set_led(uint8_t,uint8_t);
+void set_led_value(uint8_t);
