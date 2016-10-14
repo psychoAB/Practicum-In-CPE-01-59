8201459944c53ffa9a4f4f3ceb9f945ce7a0da0b
psychoAB <dome.2499@hotmail.com>
2016-10-14 12:20:36 +0700

    week 09: complete light-8.c

--- 09/light-8.c
+++ 09/light-8.c
@@ -4,8 +4,11 @@
 int main()
 {
     init_peripheral();
+    uint16_t lightAnalog = 0;
     while(1)
     {
-
+        lightAnalog = read_adc(PC4);
+        lightAnalog = lightAnalog >> 7;
+        set_led_value(lightAnalog);
     }
 }
